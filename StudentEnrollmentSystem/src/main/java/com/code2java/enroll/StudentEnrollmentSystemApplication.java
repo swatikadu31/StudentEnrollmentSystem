b5b139a8eae5fc09216e20b65c92c3c55535bd00
Swati Kadu <swatikadu31@gmail.com>
2025-05-12 16:57:37 +0530

    Student Enrollment System initial Commit

---
+++ StudentEnrollmentSystem/src/main/java/com/code2java/enroll/StudentEnrollmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.code2java.enroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEnrollmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEnrollmentSystemApplication.class, args);
+	}
+
+}
---
+++ StudentEnrollmentSystem/src/test/java/com/code2java/enroll/StudentEnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code2java.enroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEnrollmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
